c98be06f02c07d2bc636b0bc1caae55b17f7e608
MelinaMejia95 <melinamejia95@gmail.com>
2017-07-29 11:00:10 -0500

    Counter implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { reducers } from './reducers';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  AppComponent
+],
+imports: [
+  BrowserModule,  
+  StoreModule.forRoot(reducers)
+],
   providers: [],
   bootstrap: [AppComponent]
 })
